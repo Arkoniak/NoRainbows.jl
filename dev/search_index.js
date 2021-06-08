78c0447e6ae12561592ac224b5842f247935c814
Documenter.jl <documenter@juliadocs.github.io>
2021-06-08 13:57:52 +0000

    build based on 512465c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NoRainbows","category":"page"},{"location":"#NoRainbows","page":"Home","title":"NoRainbows","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NoRainbows.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NoRainbows]","category":"page"}]
+}
